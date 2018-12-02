c13579e9bb42d55eaa8c6bd61ad9eac16862cef6
aaltukhov <altukhov.artem.dev@gmail.com>
2018-12-02 19:24:51 +0300

    init commit

---
+++ Streams.Resources.csproj/ResourceStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams.Resources
+{
+    public class ResourceReaderStream : Stream
+    {
+        public ResourceReaderStream(Stream stream, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // if not key found yet: SeekValue();
+            // if value is not read yet: ReadFieldValue(...)
+            // else return 0;
+        }
+
+        private void SeekValue()
+        {
+            // while not end of stream read next field pair, compare with key and skip value if it is not equal to key
+        }
+
+        public override void Flush()
+        {
+            // nothing to do
+        }
+    }
+
+}
